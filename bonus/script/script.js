cca45a9817521149ee15ef3250d8b8f122a9da27
Andre050101 <agiancristiano90@gmail.com>
2024-11-04 17:05:33 +0100

    snack 6 done

--- bonus/script/script.js
+++ bonus/script/script.js
@@ -1,14 +1,5 @@
-/*Consegna:
-
--snack 5
--snack 6
--snack 7
--snack 9
-
-*/
-
 //-snack 5: Crea un array vuoto, chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
-
+/*
 const array = [];
 let n;
 
@@ -25,3 +16,17 @@ if(array.length===0)
     console.log("L'array di numeri dispari è vuoto!")
 else
     console.log("L'array di numeri dispari presenta "+array.length+" elementi, i quali sono: "+array);
+*/
+//-snack 6: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
+let N = parseInt(prompt("Inserisci numero N:"));
+
+while(isNaN(N) || N < 0){
+    N = parseInt(prompt("Numero invaido, reinserisci numero N:"));
+}
+
+for(let i = 0; i <= N; i++){
+    let cubo = Math.pow(i, 3);
+    console.log("il cubo di "+i+" è "+cubo);
+}
+
+
